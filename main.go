5a40fc0cb002c06e60891055bf42cc32975d49a8
InventorXtreme <inventorxtreme@gmail.com>
2023-04-21 20:15:42 -0400

    why

--- main.go
+++ main.go
@@ -6,13 +6,36 @@ import (
 )
 
 type WindowSize struct {
-	width int
-	height int
+	width int32
+	height int32
 }
 
 func main() {
-	const MAIN_WINDOW_SIZE = WindowSize{800,600}
+	MAIN_WINDOW_SIZE := WindowSize{800,600}
 
 	fmt.Println("Started")
-	sdl.Init(sdl.INIT_EVERYTHING)
-}
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+	fmt.Println("SDL INIT DONE")
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,MAIN_WINDOW_SIZE.width, MAIN_WINDOW_SIZE.height, sdl.WINDOW_SHOWN)
+
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+
+	renderer,err := sdl.CreateSoftwareRenderer(surface)
+	if err != nil {
+		panic(err)
+	}
+	rendertex := renderer.CreateTexture(sdl.PIXELFORMAT_ARGB8888,sdl.TEXTUREACCESS_STREAMING,MAIN_WINDOW_SIZE.width,MAIN_WINDOW_SIZE.height)
+
+	}
