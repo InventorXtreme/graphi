f47eec7733acf2a10cd53faaa308da293a85f32f
InventorXtreme <inventorxtreme@gmail.com>
2023-04-21 21:36:28 -0400

    Hehe

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/veandco/go-sdl2/sdl"
+	"unsafe"
 )
 
 type WindowSize struct {
-	width int32
+	width  int32
 	height int32
 }
 
+func PixelSet(pixarray []byte, pitch int, x int, y int, r, g, b, a byte) {
+	index := y*(pitch) + x*4
+	pixarray[index] = r
+	pixarray[index+1] = g
+	pixarray[index+2] = b
+	pixarray[index+3] = a
+
+}
+
 func main() {
-	MAIN_WINDOW_SIZE := WindowSize{800,600}
+	MAIN_WINDOW_SIZE := WindowSize{800, 600}
 
 	fmt.Println("Started")
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
@@ -20,7 +30,7 @@ func main() {
 	defer sdl.Quit()
 	fmt.Println("SDL INIT DONE")
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,MAIN_WINDOW_SIZE.width, MAIN_WINDOW_SIZE.height, sdl.WINDOW_SHOWN)
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, MAIN_WINDOW_SIZE.width, MAIN_WINDOW_SIZE.height, sdl.WINDOW_SHOWN)
 
 	if err != nil {
 		panic(err)
@@ -32,10 +42,32 @@ func main() {
 		panic(err)
 	}
 
-	renderer,err := sdl.CreateSoftwareRenderer(surface)
+	renderer, err := sdl.CreateSoftwareRenderer(surface)
 	if err != nil {
 		panic(err)
 	}
-	rendertex := renderer.CreateTexture(sdl.PIXELFORMAT_ARGB8888,sdl.TEXTUREACCESS_STREAMING,MAIN_WINDOW_SIZE.width,MAIN_WINDOW_SIZE.height)
-
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ARGB8888, sdl.TEXTUREACCESS_STREAMING, MAIN_WINDOW_SIZE.width, MAIN_WINDOW_SIZE.height)
+	if err != nil {
+		panic(err)
 	}
+	pixels, pitch, err := tex.Lock(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for true {
+		//for u := 0; u < int(MAIN_WINDOW_SIZE.width); u++ {
+		//	for v := 0; v < int(MAIN_WINDOW_SIZE.height); v++ {
+		//		PixelSet(pixels, pitch, u, v, 255, 0, 255, 0)
+		//	}
+		//}
+		PixelSet(pixels, pitch, 0, 0, 255, 0, 255, 000)
+
+		tex.Update(nil, unsafe.Pointer(&pixels[0]), pitch)
+
+		tex.Unlock()
+		renderer.Copy(tex, nil, nil)
+		window.UpdateSurface()
+	}
+
+}
